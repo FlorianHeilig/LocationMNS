2fdb7dcc4a9e09dd8492cb079db9a49dc6174fbd
FlorianHeilig <florian.heilig0@gmail.com>
2025-10-23 19:02:10 +0200

    Improves header and adds logo
    Adds a logo and improves the overall appearance of the header, including styling and structure.
    Also adds react-icons for future use.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,19 @@
 import style from "../assets/header.module.css";
 import button from "../assets/button.module.css";
+import logo from "../assets/Logo_LocMNS.png";
+import { GoChevronDown } from "react-icons/go";
 
 export default function Header() {
   return (
     <div className={style.header}>
+      <div className={style.brand}>
+        <img src={logo} alt="Logo LocMNS" className={style.logo} />
+        <h1 className={style.title}>LocMNS</h1>
+        <a></a>
+        <a href="">
+          Catégorie <GoChevronDown />
+        </a>
+      </div>
       <button className={button.borderButton}>Inscription</button>
     </div>
   );
