b5e042ad05e620a5fe0b55d9c7fbdcd4ea7816af
FlorianHeilig <florian.heilig0@gmail.com>
2025-10-25 06:11:27 +0200

    feat: Introduce Login component

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,9 @@
+import button from "../assets/button.module.css";
+
+export default function Login() {
+  return (
+    <>
+      <button className={button.borderButton}>Inscription</button>
+    </>
+  );
+}
