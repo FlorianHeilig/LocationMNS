9d2a9739a44c7c915db1e023a4ad8203fb3dec86
FlorianHeilig <florian.heilig0@gmail.com>
2025-10-25 06:11:27 +0200

    refactor(header): Integrate Brand and Login components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,14 @@
 import style from "../assets/header.module.css";
-import button from "../assets/button.module.css";
-import logo from "../assets/Logo_LocMNS.png";
 import Nav from "./Nav";
+import Brand from "./Brand";
+import Login from "./Login";
 
 export default function Header() {
   return (
     <div className={style.header}>
-      <div className={style.brand}>
-        <img src={logo} alt="Logo LocMNS" className={style.logo} />
-        <h1 className={style.title}>LocMNS</h1>
-        <Nav />
-      </div>
-      <button className={button.borderButton}>Inscription</button>
+      <Brand />
+      <Nav />
+      <Login />
     </div>
   );
 }
