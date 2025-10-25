877296374ceef6bfa2f310e18c5f38aff71c17ec
FlorianHeilig <florian.heilig0@gmail.com>
2025-10-25 06:11:28 +0200

    refactor(nav): Rework navigation component and styles for hover-dropdown

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,9 +1,26 @@
-import NavSection from "./NavSection";
+import { useState } from "react";
+import style from "../assets/nav.module.css";
+import NavSectionWindowCategory from "./NavSectionWindowCategory";
+import { CgChevronRight } from "react-icons/cg";
 
 export default function Nav() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <>
-      <NavSection />
-    </>
+    <div className={style.nav}>
+      <div
+        className={style.sectionContainer}
+        onMouseEnter={() => setOpen(true)}
+        onMouseLeave={() => setOpen(false)}
+      >
+        <a className={style.section}>
+          Catégorie
+          <CgChevronRight className={style.icon} />
+        </a>
+        <div className={`${style.window} ${open ? style.show : ""}`}>
+          <NavSectionWindowCategory />
+        </div>
+      </div>
+    </div>
   );
 }
