b956f597fd9d7a03b8c67243b7786e424a8d1301
FlorianHeilig <florian.heilig0@gmail.com>
2025-10-25 06:11:28 +0200

    style(nav): Adjust navigation window grid and category display

--- frontend/src/components/NavSectionWindowCategory.jsx
+++ frontend/src/components/NavSectionWindowCategory.jsx
@@ -3,9 +3,9 @@ import NavSectionWindowGrid from "./NavSectionWindowGrid";
 
 export default function NavSectionWindowCategory() {
   return (
-    <div>
+    <>
       <h2>Ordinateur</h2>
       <NavSectionWindowGrid />
-    </div>
+    </>
   );
 }
